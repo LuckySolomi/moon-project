03fa92a80bca094d050ac395e138ebe27bb9f0df
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-09-30 22:50:19 +0300

    Adds Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <div>
+      <section>
+        <h1 className={styles.gradientText}>
+          A new economic primitive for funding decentralized AI
+        </h1>
+      </section>
+    </div>
+  );
+}
+
+export default Main;
