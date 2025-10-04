82902b8d8c9dc64bd7d66dda380e5083d8fafcb3
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-04 23:37:36 +0300

    Updates app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import BackgroundElements from "./components/BackgroundElements/BackgroundElements";
 import Header from "./components/Header/Header";
 import InitialSection from "./components/InitialSection/InitialSection.jsx";
 import NumberCardSection from "./components/NumberCardSection/NumberCardSection.jsx";
+import RunningLineSection from "./components/RunningLineSection/RunningLineSection.jsx";
+import DescriptionSection from "./components/DescriptionSection/DescriptionSection.jsx";
+import TableSection from "./components/TableSection/TableSection.jsx";
 import styles from "./App.module.css";
 
 function App() {
+  const numberSectionRef = useRef(null);
+  const isNumberSectionInView = useInView(numberSectionRef, { threshold: 0.3 });
   return (
     <div className={styles.app}>
       <BackgroundElements />
       <Header />
       <main>
-        <InitialSection />
-        <NumberCardSection />
+        <InitialSection gradientChanged={isNumberSectionInView} />
+        <NumberCardSection ref={numberSectionRef} />
+        <RunningLineSection />
+        <DescriptionSection />
+        <TableSection />
       </main>
     </div>
   );
