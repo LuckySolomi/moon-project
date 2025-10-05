e4fb9129c5b97ef06172f4f82106a75c5b1d0af5
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-05 22:57:24 +0300

    Adds rocket animation under the table

--- src/components/TableSection/TableSection.jsx
+++ src/components/TableSection/TableSection.jsx
@@ -4,13 +4,21 @@ import TableRow from "../ToolkitsComponents/TableRow/TableRow";
 import Button from "../ToolkitsComponents/Button/Button.jsx";
 import ButtonLink from "../ToolkitsComponents/ButtonLink/ButtonLink.jsx";
 import ArrowIcon from "../ToolkitsComponents/ArrowIcon/ArrowIcon.jsx";
+import RocketIcon from "../../assets/img/rocket.svg";
 import styles from "./TableSection.module.css";
 
 function TableSection() {
   const [showAll, setShowAll] = useState(false);
   const visibleData = showAll ? tableData : tableData.slice(0, 10);
 
-  const handleToggle = () => setShowAll((prev) => !prev);
+  const [launchRocket, setLaunchRocket] = useState(false);
+
+  const handleToggle = () => {
+    setShowAll((prev) => !prev);
+    setLaunchRocket(true);
+    // зупинити анімацію після завершення (щоб можна було повторити)
+    setTimeout(() => setLaunchRocket(false), 4000); // — тривалість анімації
+  };
 
   return (
     <section>
@@ -23,7 +31,7 @@ function TableSection() {
         test LLMs on a large number of different evaluation tasks. The higher
         the score, the better the LLM.
       </p>
-      <div>
+      <div className={styles.tableSectionTableContainer}>
         {/* Заголовок */}
         <TableRow
           isHeader
@@ -51,6 +59,10 @@ function TableSection() {
           />
         ))}
       </div>
+      {launchRocket && (
+        <img src={RocketIcon} alt="Rocket" className={styles.rocket} />
+      )}
+
       <div className={styles.tableSectionButtonContainer}>
         <ButtonLink onClick={handleToggle}>
           {showAll ? "View less" : "View more"}
