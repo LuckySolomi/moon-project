32961140dee959b4fbf8f97e30b615fb963868ba
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-06 12:06:12 +0300

    Adds footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ButtonLink from "../ToolkitsComponents/ButtonLink/ButtonLink.jsx";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className={styles.footerButtonContainer}>
+        <ButtonLink>How It Works</ButtonLink>
+        <ButtonLink>Buy Salt AI</ButtonLink>
+      </div>
+      <div className={styles.footerBottom}>
+        <div className={styles.footerIconsContainer}>
+          <img src="./src/assets/img/telegram.svg" alt="telegram" />
+          <img src="./src/assets/img/x-icon.svg" alt="x-icon" />
+        </div>
+        <nav className={styles.footerNavContainer}>
+          <a href="#">Terms of Use</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Cookie Policy</a>
+        </nav>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
