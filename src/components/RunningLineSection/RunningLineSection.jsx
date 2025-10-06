d705d1b47238ca62072534a290b3b9c20feb180a
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-06 22:44:31 +0300

    Fixes running block with icons in RunningLineSection

--- src/components/RunningLineSection/RunningLineSection.jsx
+++ src/components/RunningLineSection/RunningLineSection.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import styles from "./RunningLineSection.module.css";
 
 function RunningLineSection() {
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { threshold: 0.3 });
+
   return (
-    <section className={styles.runningLineSection}>
+    <section className={styles.runningLineSection} ref={sectionRef}>
       <h2>Projects integrated into the Arrakis AI Ecosystem</h2>
-      <div className={styles.logosContainer}>
+
+      <motion.div
+        className={styles.logosContainer}
+        initial={{ x: "100%" }}
+        animate={isInView ? { x: "0%" } : { x: "100%" }}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
         <img src="solana.svg" alt="Solana" />
         <img src="arweave.svg" alt="Arweave" />
         <img src="bittensor.svg" alt="Bittensor" />
-        <img src="logo4.svg" alt="Logo4" />
-        <img src="telegram.svg" alt="Logo5" />
-      </div>
+        <img className={styles.roundIcons} src="logo4.svg" alt="Logo4" />
+        <img className={styles.roundIcons} src="telegram.svg" alt="Logo5" />
+      </motion.div>
     </section>
   );
 }
