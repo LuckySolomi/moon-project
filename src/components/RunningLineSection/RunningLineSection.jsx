cf217fe4d847712923f6e6b828c0529e64a28148
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-06 17:40:23 +0300

    Adapts RunningLineSection to different devices

--- src/components/RunningLineSection/RunningLineSection.jsx
+++ src/components/RunningLineSection/RunningLineSection.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-("use client");
-import { motion, useScroll, useTransform } from "framer-motion";
-import Button from "../ToolkitsComponents/Button/Button.jsx";
 import styles from "./RunningLineSection.module.css";
 
 function RunningLineSection() {
-  const { scrollYProgress } = useScroll();
-
-  // рухаємо блок від translateX(100%) до translateX(-100%)
-  const x = useTransform(scrollYProgress, [0.2, 0.5], ["100%", "-100%"]);
   return (
     <section className={styles.runningLineSection}>
       <h2>Projects integrated into the Arrakis AI Ecosystem</h2>
-      <motion.div className={styles.logosContainer} style={{ x }}>
+      <div className={styles.logosContainer}>
         <img src="./src/assets/img/solana.svg" alt="Solana" />
         <img src="./src/assets/img/arweave.svg" alt="Arweave" />
         <img src="./src/assets/img/bittensor.svg" alt="Bittensor" />
         <img src="./src/assets/img/logo4.svg" alt="Logo4" />
         <img src="./src/assets/img/telegram.svg" alt="Logo5" />
-      </motion.div>
+      </div>
     </section>
   );
 }
